35e15c39ed63962b0ca94025eb4cff9702cae07e
Botond Tanczos <botond.tanczos@aplnexted.com>
2025-06-19 12:43:37 +0300

    Initial commit

---
+++ src/LegoEventSignup/LegoEventSignupStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace LegoEventSignup
+{
+    public class LegoEventSignupStack : Stack
+    {
+        internal LegoEventSignupStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
