f36de7097e531ba20acaedcfdd662de00336b812
Botond Tanczos <botond.tanczos@aplnexted.com>
2025-06-25 07:22:47 +0300

    Added IDesignTimeDbContextFactory

---
+++ lambda/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace EventSignup.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<EventSignupContext>
+    {
+        public EventSignupContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<EventSignupContext>();
+            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+
+            return new EventSignupContext(optionsBuilder.Options);
+        }
+    }
+}
